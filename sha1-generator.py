b6939a46237defe75e1d83632d222eb4ab0757a7
Maximilian Herkender <static@brokenfunction.com>
2012-03-18 14:13:32 -0700

    reuse result of operation on a variable, so that variable doesn't need to be GetLocal'd twice

--- sha1-generator.py
+++ sha1-generator.py
@@ -13,11 +13,17 @@ for j in xrange(0, 80):
       k = 0xCA62C1D6
     if j < 16:
       print "var w%s:uint = byteInput.readUnsignedInt();" % j
+      print "tmp = ((a << 5) | (a >>> 27)) + (%s) + e + 0x%08X + w%s; " \
+        "e = d; " \
+        "d = c; " \
+        "c = (b << 30) | (b >>> 2); " \
+        "b = a; " \
+        "a = tmp;" % (f, k, j % 16)
     else:
-      print "w%s ^= w%s ^ w%s ^ w%s; w%s = (w%s << 1) | (w%s >>> 31);" % (j % 16, (j - 3) % 16, (j - 8) % 16, (j - 14) % 16, j % 16, j % 16, j % 16)
-    print "tmp = ((a << 5) | (a >>> 27)) + (%s) + e + 0x%08X + w%s; " \
-      "e = d; " \
-      "d = c; " \
-      "c = (b << 30) | (b >>> 2); " \
-      "b = a; " \
-      "a = tmp;" % (f, k, j % 16)
+      print "w%s ^= w%s ^ w%s ^ w%s;" % (j % 16, (j - 3) % 16, (j - 8) % 16, (j - 14) % 16)
+      print "tmp = ((a << 5) | (a >>> 27)) + (%s) + e + 0x%08X + (w%s = (w%s << 1) | (w%s >>> 31)); " \
+        "e = d; " \
+        "d = c; " \
+        "c = (b << 30) | (b >>> 2); " \
+        "b = a; " \
+        "a = tmp;" % (f, k, j % 16, j % 16, j % 16)
