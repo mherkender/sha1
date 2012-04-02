138e2ba5acca70e2de98338b099bbbb0a0f3f00f
Maximilian Herkender <static@brokenfunction.com>
2012-04-01 17:22:13 -0700

    convert w array to individual local variables

--- sha1-generator.py
+++ sha1-generator.py
@@ -11,7 +11,11 @@ for j in xrange(0, 80):
     else:
       f = "b ^ c ^ d"
       k = 0xCA62C1D6
-    print "tmp = ((a << 5) | (a >>> 27)) + (%s) + e + 0x%08X + w[%s]; " \
+    if j < 16:
+      print "var w%s:uint = strToInt(input, i + %s);" % (j, j * 4)
+    else:
+      print "var w%s:uint = w%s ^ w%s ^ w%s ^ w%s; w%s = (w%s << 1) | (w%s >>> 31);" % (j, j - 3, j - 8, j - 14, j - 16, j, j, j)
+    print "tmp = ((a << 5) | (a >>> 27)) + (%s) + e + 0x%08X + w%s; " \
       "e = d; " \
       "d = c; " \
       "c = (b << 30) | (b >>> 2); " \
